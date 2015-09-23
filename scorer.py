864d183eefaa41e055cc40a8466d1047edf679c0
Mitch Rule <mitchman5506@gmail.com>
2015-09-24 01:06:52 +1000

    Added and began concepts

---
+++ scorer.py
@@ -0,0 +1,11 @@
+'''
+Author = Mitch Rule
+Email = mitchman5506@gmail.com
+
+This program will keep score of a game of darts for two players.
+Each player has to count their score down from 501 and reach zero on 
+a double number.
+Players will enter their throws into the terminal with a specific syntax
+and the scorer calculate their score and eventually determine a winner.
+'''
+
