4e8226940a42fa4017126ee9456365ce0e939150
Mitch Rule <mitchman5506@gmail.com>
2016-06-22 18:41:16 +1000

    Added to valid throw test

--- scorer.py
+++ scorer.py
@@ -12,79 +12,78 @@ import re
 
 
 class Player(object):
+    '''
+    Contains all of the data for each player
+    and the functions for calculating scores.
+    '''
 
-	'''
-	Contains all of the data for each player
-	and the functions for calculating scores.
-	'''
-
-	def __init__(self, name):
-		self.name = name
-		self.score = 501
-		self.throwlist = []
-
-	# TODO Allow final throw to be a double to finish game.
-	# def throw(throws):
-	# 	if isValidThrow(throws):
-	# 		for throw in scoreConvert(throws):
-	# 			if self.score - throw > 1
-	# 				self.score = self.score - throw
-	# 				self.throwlist.append(throw)
+    def __init__(self, name):
+        self.name = name
+        self.score = 501
+        self.throwlist = []
 
 
 def scoreConvert(throws):
+    '''
+    Takes inputs as a list of three string throws.
+    Applies triple and double multipliers.
 
-	'''
-	Takes inputs as a list of three string throws.
-	Applies triple and double multipliers.
+    Returns throws as list of integers
 
-	Returns throws as list of integers
-	
-	Example: ['T20', 'D20', '15'] --> [60, 40, 15]
-	'''
+    Example: ['T20', 'D20', '15'] --> [60, 40, 15]
+    '''
 
-	convertedThrows = []
+    convertedThrows = []
 
-	# TODO Add bullseye
-	for throw in throws:
-		if 'd' in throw.lower():
-			# Strips character from the start of string to get number only
-			strippedThrow = re.sub(r'd', '', throw.lower())
-			throw = int(strippedThrow) * 2
-			convertedThrows.append(throw)
+    # TODO Add bullseye
+    for throw in throws:
+        if 'd' in throw.lower():
+            # Strips character from the start of string to get number only
+            strippedThrow = re.sub(r'd', '', throw.lower())
+            throw = int(strippedThrow) * 2
+            convertedThrows.append(throw)
 
-			print(strippedThrow)
-			print('Double score')
-		elif 't' in throw.lower():
-			strippedThrow = re.sub(r't', '', throw.lower())
-			throw = int(strippedThrow) * 3
-			convertedThrows.append(throw)
+            print(strippedThrow)
+            print('Double score')
+        elif 't' in throw.lower():
+            strippedThrow = re.sub(r't', '', throw.lower())
+            throw = int(strippedThrow) * 3
+            convertedThrows.append(throw)
 
-			print(strippedThrow)
-			print('Triple score')
-		else:
-			print('Single score')
-			throw = int(throw)
-			convertedThrows.append(throw)
+            print(strippedThrow)
+            print('Triple score')
+        else:
+            print('Single score')
+            throw = int(throw)
+            convertedThrows.append(throw)
 
-	return convertedThrows
+    return convertedThrows
+
 
 def isValidThrow(throws):
+    '''
+    Takes throws and returns True or False to
+    whether they are legal throws.
 
-	'''
-	Takes throws and returns True or False to
-	whether they are legal throws. 
+    Example: ['T21', '20', '20'] --> False
+    '''
 
-	Example: ['T21', '20', '20'] --> False
-	'''
+    valid = True
+    validThrows = []
 
-	valid = True
+    for i in range(1, 21):
+        validThrows.append(str(i))
+        validThrows.append('d' + str(i))
+        validThrows.append('t' + str(i))
 
-	for throw in throws:
-		if re.match(r'^[dt][1-20]'|r'^[1-20]', throw.lower()) or (throw.lower() == 'b') or (throw.lower() == 'db'):
-			pass
-		else:
-			valid = False
-			break
+    validThrows.append('25')
+    validThrows.append('50')
 
-	return valid
+    for throw in throws:
+        if throw.lower() in validThrows:
+            pass
+        else:
+            valid = False
+            break
+
+    return valid
--- scorer_test.py
+++ scorer_test.py
@@ -2,8 +2,9 @@ from scorer import *
 
 def test_Validator():
     assert isValidThrow(['20', '20', '20']) == True
-    assert isValidThrow(['B', '20', '20']) == True
-    assert isValidThrow(['DB', 'D20', 'T19']) == True
+    assert isValidThrow(['25', '20', '20']) == True
+    assert isValidThrow(['50', 'D20', 'T19']) == True
     assert isValidThrow(['23', '40', '1']) == False
     assert isValidThrow(['Y20', '12', '1']) == False
     assert isValidThrow(['T21', 'T1', '16']) == False
+    assert isValidThrow(['21', '13', '1']) == False
